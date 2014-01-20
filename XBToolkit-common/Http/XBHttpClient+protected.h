6a21430dd2d5d67b882afdedbccc44fb9c40c3b6
Alexis Kinsella <alexis.kinsella@gmail.com>
2014-01-20 10:44:19 +0100

    Fixing name of XBHttpClient related Blocks

--- XBToolkit-common/Http/XBHttpClient+protected.h
+++ XBToolkit-common/Http/XBHttpClient+protected.h
@@ -2,11 +2,6 @@
 
 @interface XBHttpClient (protected)
 
-@property(nonatomic, strong) NSString *baseUrl;
-
-- (NSString *)URLWithPath:(NSString *)path method:(NSString *)method parameters:(NSDictionary *)parameters
-{
-    return [[NSURL URLWithString:path relativeToURL:self.HTTPRequestOperationManager.baseURL] absoluteString];
-}
+- (NSString *)URLWithPath:(NSString *)path method:(NSString *)method parameters:(NSDictionary *)parameters;
 
 @end
